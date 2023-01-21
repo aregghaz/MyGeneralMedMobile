bbacbb28f1a495c40aeff68f71403a950628c8e8
Areg <aregghaz@gmail.com>
2023-01-21 16:04:30 +0400

    crudkey bug

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,3 +1,4 @@
+
 /**
  * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
  * https://reactnavigation.org/docs/getting-started
@@ -45,28 +46,28 @@ function RootNavigator() {
     useEffect(() => {
         checkUser()
     }, [])
-    const checkUser = async () => {
+    const  checkUser = async () => {
         try {
             const userData = await AuthApi.getUser()
             setUser(userData)
-        } catch (e) {
+        }catch (e){
             setUser(null)
         }
     }
-    // if (user === null) {
-    //     return (<View style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
-    //                 <ActivityIndicator/>
-    //             </View>)
-    // }
+    if (user === null) {
+        return (<View style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
+            <ActivityIndicator/>
+        </View>)
+    }
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-            <Stack.Screen name="Home" component={BottomTabNavigator} options={{headerShown: false}}/>
-            <Stack.Screen name="NotFound" component={NotFoundScreen} options={{title: 'Oops!'}}/>
-            <Stack.Group screenOptions={{presentation: 'modal'}}>
-                <Stack.Screen name="Modal" component={ModalScreen}/>
-            </Stack.Group>
-
+            {user? <>
+                <Stack.Screen name="Home" component={BottomTabNavigator} options={{headerShown: false}}/>
+                <Stack.Screen name="NotFound" component={NotFoundScreen} options={{title: 'Oops!'}}/>
+                <Stack.Group screenOptions={{presentation: 'modal'}}>
+                    <Stack.Screen name="Modal" component={ModalScreen}/>
+                </Stack.Group>
+            </> : <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>}
         </Stack.Navigator>
     );
 }
@@ -137,145 +138,3 @@ function TabSettingsIcon(props: {
 }) {
     return <SettingsComponent/>;
 }
-
-
-//
-// /**
-//  * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
-//  * https://reactnavigation.org/docs/getting-started
-//  *
-//  */
-// import {FontAwesome} from '@expo/vector-icons';
-// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import * as React from 'react';
-// import {ActivityIndicator, ColorSchemeName, Pressable} from 'react-native';
-// import ClientComponent from '../assets/images/Clients';
-// import Colors from '../constants/Colors';
-// import useColorScheme from '../hooks/useColorScheme';
-// import ModalScreen from '../screens/ModalScreen';
-// import NotFoundScreen from '../screens/NotFoundScreen';
-// import Clients from '../screens/Clients';
-// import TabTwoScreen from '../screens/TabTwoScreen';
-// import {RootStackParamList, RootTabParamList, RootTabScreenProps} from '../types';
-// import LinkingConfiguration from './LinkingConfiguration';
-// import SettingsComponent from "../assets/images/Settings";
-// import Login from "../screens/LoginScreen";
-// import {useEffect, useState} from "react";
-// import {AuthApi} from "../api/auth";
-// import {View} from "react-native";
-//
-// export default function Navigation({colorScheme}: { colorScheme: ColorSchemeName }) {
-//     return (
-//         <NavigationContainer
-//             linking={LinkingConfiguration}
-//             theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-//             <RootNavigator/>
-//         </NavigationContainer>
-//     );
-// }
-//
-// /**
-//  * A root stack navigator is often used for displaying modals on top of all other content.
-//  * https://reactnavigation.org/docs/modal
-//  */
-// const Stack = createNativeStackNavigator<RootStackParamList>();
-//
-// function RootNavigator() {
-//     const [user, setUser] = useState<Object>({});
-//     useEffect(() => {
-//         checkUser()
-//     }, [])
-//     const  checkUser = async () => {
-//         try {
-//             const userData = await AuthApi.getUser()
-//             setUser(userData)
-//         }catch (e){
-//             setUser({})
-//         }
-//     }
-//     if (Object.values(user).length === 0) {
-//         return (<View style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
-//             <ActivityIndicator/>
-//         </View>)
-//     }
-//     return (
-//         <Stack.Navigator>
-//             {Object.values(user).length > 0 ? <>
-//                 <Stack.Screen name="Home" component={BottomTabNavigator} options={{headerShown: false}}/>
-//                 <Stack.Screen name="NotFound" component={NotFoundScreen} options={{title: 'Oops!'}}/>
-//                 <Stack.Group screenOptions={{presentation: 'modal'}}>
-//                     <Stack.Screen name="Modal" component={ModalScreen}/>
-//                 </Stack.Group>
-//             </> : <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>}
-//         </Stack.Navigator>
-//     );
-// }
-//
-// /**
-//  * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
-//  * https://reactnavigation.org/docs/bottom-tab-navigator
-//  */
-// const BottomTab = createBottomTabNavigator<RootTabParamList>();
-//
-//
-// function BottomTabNavigator() {
-//     const colorScheme = useColorScheme();
-//
-//     return (
-//         <BottomTab.Navigator
-//             initialRouteName="Clients"
-//             screenOptions={{
-//                 tabBarActiveTintColor: Colors[colorScheme].tint,
-//             }}>
-//             <BottomTab.Screen
-//                 name="Clients"
-//                 component={Clients}
-//                 options={({navigation}: RootTabScreenProps<'Clients'>) => ({
-//                     title: 'Clients',
-//                     tabBarIcon: ({color}) => <TabClientIcon name="code" color={color}/>,
-//                     headerRight: () => (
-//                         <Pressable
-//                             onPress={() => navigation.navigate('Modal')}
-//                             style={({pressed}) => ({
-//                                 opacity: pressed ? 0.5 : 1,
-//                             })}>
-//                             <FontAwesome
-//                                 name="info-circle"
-//                                 size={25}
-//                                 color={Colors[colorScheme].text}
-//                                 style={{marginRight: 15}}
-//                             />
-//                         </Pressable>
-//                     ),
-//                 })}
-//             />
-//             <BottomTab.Screen
-//                 name="TabTwo"
-//                 component={TabTwoScreen}
-//                 options={{
-//                     title: 'Settings',
-//                     tabBarIcon: ({color}) => <TabSettingsIcon name="code" color={color}/>,
-//                 }}
-//             />
-//         </BottomTab.Navigator>
-//     );
-// }
-//
-// /**
-//  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-//  */
-// function TabClientIcon(props: {
-//     name: React.ComponentProps<typeof FontAwesome>['name'];
-//     color: string;
-// }) {
-//     return <ClientComponent/>;
-// }
-//
-// function TabSettingsIcon(props: {
-//     name: React.ComponentProps<typeof FontAwesome>['name'];
-//     color: string;
-// }) {
-//     return <SettingsComponent/>;
-// }
